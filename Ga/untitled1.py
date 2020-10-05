e75f8deef3ef530030f91f384001ac11bfd892e4
Antonio Mejia <avmejia@tec.mx>
2020-10-05 14:05:52 -0500

    051020

---
+++ Ga/untitled1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 10:13:02 2020
+
+@author: avmejia
+"""
+
+from random import seed
+from random import random
+from random import randint
+
+seed(1)
+print(randint(1,100))
+print(randint(1,10))
+print(randint(1,10))
+
